a6f04d2d81371dc11d6a2c80dc328a0ed9f166ec
syrekable <jordan2000niedziela@gmail.com>
2020-01-23 16:34:06 +0100

    Angle can be 0 now

--- CORDIC2.py
+++ CORDIC2.py
@@ -1,39 +1,47 @@
-import numpy
+from numpy import pi, sin, cos, arctan, sqrt
 
 def get_angle(beta:str):
-    beta = beta.split("/")
-    return numpy.pi/int(beta[1])
-
+    if beta != "0":
+        beta = beta.split("/")
+        return pi/int(beta[1])
+    else:
+        return 0
+    
 def cordic_trig(beta,N=40):
+    ##the error factor for the number of iterations (rotations)
     def K_vals(n):
         K = []
         acc = 1.0
         for i in range(0, n):
-            acc = acc * (1.0/numpy.sqrt(1 + 2.0**(-2*i)))
+            acc = acc * (1.0/sqrt(1 + 2.0**(-2*i)))
             K.append(acc)
         return K
 
 
     K = K_vals(N)
-    atans = [numpy.arctan(2.0**(-i)) for i in range(0,N)]
+    atans = [arctan(2.0**(-i)) for i in range(0,N)]
     x = 1
     y = 0
-
+    
     for i in range(0,N):
+        ##flipping sign for certain quadrants
         d = 1.0
         if beta < 0:
             d = -1.0
 
         (x, y) = (x - (d * (2.0 ** (-i)) * y), (d * (2.0 ** (-i)) * x) + y)
-        beta = beta - (d*numpy.arctan(2**(-i)))
+        beta = beta - (d*arctan(2**(-i)))
     return K*x, K*y
 
 if __name__ == '__main__':
     beta, n = input("Angle in radians, num of iterations like (ex. pi/2 30): ").split()
     beta, n = get_angle(beta), int(n)
 
-    print("Actual cos(%f) = %f" % (beta, numpy.cos(beta)))
-    print ("Actual sin(%f) = %f\n" % (beta, numpy.sin(beta)))
+    print("{0} Pi approximation: {1} {2}".format(5*"=",pi,5*"="))
+    print("Actual cos(%f) = %f" % (beta, cos(beta)))
+    print("Actual sin(%f) = %f\n" % (beta, sin(beta)))
+
+    print("CORDIC in the works")
 
 ##    cos_val, sin_val = cordic_trig(beta,3)
 ##    
