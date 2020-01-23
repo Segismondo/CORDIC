473cf48d39a1bae4063bccb397cc414482611391
syrekable <jordan2000niedziela@gmail.com>
2020-01-23 15:49:02 +0100

    Added user input handling, CORDIG itself unusable for now

--- CORDIC2.py
+++ CORDIC2.py
@@ -1,18 +1,21 @@
-import math
+import numpy
 
+def get_angle(beta:str):
+    beta = beta.split("/")
+    return numpy.pi/int(beta[1])
 
 def cordic_trig(beta,N=40):
     def K_vals(n):
         K = []
         acc = 1.0
         for i in range(0, n):
-            acc = acc * (1.0/math.sqrt(1 + 2.0**(-2*i)))
+            acc = acc * (1.0/numpy.sqrt(1 + 2.0**(-2*i)))
             K.append(acc)
         return K
 
 
-    K = 0.6072
-    atans = [math.atan(2.0**(-i)) for i in range(0,N)]
+    K = K_vals(N)
+    atans = [numpy.arctan(2.0**(-i)) for i in range(0,N)]
     x = 1
     y = 0
 
@@ -22,15 +25,17 @@ def cordic_trig(beta,N=40):
             d = -1.0
 
         (x, y) = (x - (d * (2.0 ** (-i)) * y), (d * (2.0 ** (-i)) * x) + y)
-        beta = beta - (d*math.atan(2**(-i)))
+        beta = beta - (d*numpy.arctan(2**(-i)))
     return K*x, K*y
 
 if __name__ == '__main__':
-    beta = 1
-    print("Actual cos(%f) = %f" % (beta, math.cos(beta)))
-    print ("Actual sin(%f) = %f\n" % (beta, math.sin(beta)))
+    beta, n = input("Angle in radians, num of iterations like (ex. pi/2 30): ").split()
+    beta, n = get_angle(beta), int(n)
 
-    cos_val, sin_val = cordic_trig(beta)
-    
-    print ("CORDIC cos(%f) = %f" % (beta, cos_val))
-    print ("CORDIC sin(%f) = %f" % (beta, sin_val))
+    print("Actual cos(%f) = %f" % (beta, numpy.cos(beta)))
+    print ("Actual sin(%f) = %f\n" % (beta, numpy.sin(beta)))
+
+##    cos_val, sin_val = cordic_trig(beta,3)
+##    
+##    print ("CORDIC cos(%f) = %f" % (beta, cos_val))
+##    print ("CORDIC sin(%f) = %f" % (beta, sin_val))
