de23d4bab377520ccc3c59dcc6f2396c99e946bd
syrekable <jordan2000niedziela@gmail.com>
2020-01-23 17:20:12 +0100

    Everything works as expected

--- CORDIC.py
+++ CORDIC.py
@@ -7,30 +7,27 @@ def get_angle(beta:str):
     else:
         return 0
     
-def cordic(beta,N=40):
+def cordic(beta:float,N:int):
     ##the error factor for the number of iterations (rotations)
-    def K_vals(n):
-        K = []
-        acc = 1.0
+    def error(n):
+        K = 1.0
         for i in range(0, n):
-            acc = acc * (1.0/sqrt(1 + 2.0**(-2*i)))
-            K.append(acc)
+            K = K * (1.0/sqrt(1 + 2.0**(-2*i)))
         return K
 
-    K = K_vals(N)
-    ##sin(a) + cos(a) for a = 0
-    x, y = 1, 0
-    
-    ##the main loop
+    ##(sin(a) + cos(a)) tuple for a = 0, error after N iterations
+    point, K = (1, 0), error(N)
+    ##the main loop (basically binary search, lol)
     for i in range(0,N):
         ##go (counter)clockwise, because the desired angle is (larger) smaller
         d = 1.0
         if beta < 0:
             d = -1.0
-        (x, y) = (x - (d * (2.0 ** (-i)) * y), (d * (2.0 ** (-i)) * x) + y)
-        beta = beta - (d*arctan(2**(-i)))
+        x, y    = point[0], point[1]
+        point   = (x - (d * (2.0 ** (-i))) * y, d * (2.0 ** (-i)) * x + y)
+        beta    = beta - (d*arctan(2**(-i)))
         
-    return K*x, K*y
+    return K*point[0], K*point[1]
 
 if __name__ == '__main__':
     beta, n = input("Angle in radians, num of iterations like (ex. pi/2 30): ").split()
@@ -40,9 +37,9 @@ if __name__ == '__main__':
     print("Actual cos(%f) = %f" % (beta, cos(beta)))
     print("Actual sin(%f) = %f\n" % (beta, sin(beta)))
 
-    print("CORDIC in the works")
+##    print("CORDIC in the works")
 
-##    cos_val, sin_val = cordic(beta,3)
-##    
-##    print ("CORDIC cos(%f) = %f" % (beta, cos_val))
-##    print ("CORDIC sin(%f) = %f" % (beta, sin_val))
+    cos_val, sin_val = cordic(beta,n)
+    
+    print ("CORDIC cos(%f) = %f" % (beta, cos_val))
+    print ("CORDIC sin(%f) = %f" % (beta, sin_val))
