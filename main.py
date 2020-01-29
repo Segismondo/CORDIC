61e20c496332eb53913c9c432a2a637df16fc19d
syrekable <jordan2000niedziela@gmail.com>
2020-01-29 12:43:52 +0100

    Showing them coordinates

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import CORDIC
 import plotter
 from decimal import Decimal
 
-'''the angle is correct, but now CORDIC fucks up somehow'''
 beta,n, = CORDIC.menu()
 cos_init, sin_init = CORDIC.init(beta)
 
@@ -18,10 +17,10 @@ plotter.plot_line(cos_init,sin_init,style='o:g')
 
 for i in range(0,n):
     cos,sin = CORDIC.cordic(beta, i+1)
-    print("iteration = %d\nsin = %f\ncos = %f\n"% (i+1,Decimal(sin), Decimal(cos)))
-    plotter.plot_line(cos,sin)
+    print("iteration = %d\nsin = %f\ncos = %f\n"% (i+1,sin, cos))
+    plotter.plot_line(cos,sin) if i < n-5 else plotter.plot_line(cos,sin,annotate=True)
 
-plotter.plot_line(Decimal(cos_init),Decimal(sin_init),style='D:g')
+plotter.plot_line(cos_init,sin_init,style='D:g',annotate=True)
 
 input("Any key to terminate...")
 
--- plotter.py
+++ plotter.py
@@ -22,9 +22,11 @@ def init():
     circle = plt.Circle(ORIGIN,RADIUS,edgecolor='k',facecolor='None')
     ax.add_patch(circle)
 
-def plot_line(x:float,y:float,style='o:r',scale=0):
-    '''draws a red line from (0,0) to (x,y) and waits 0.1 s'''
+def plot_line(x:float,y:float,style='o:r',scale=0,annotate=False):
+    '''draws a red line from (0,0) to (x,y) and waits 0.5 s'''
     line, = ax.plot((0,x),(0,y),style,scalex=scale,scaley=scale)
+    if annotate:
+        ax.annotate('(%.16f, %.16f)' % (x,y), xy=(x,y), textcoords='data') 
     plt.pause(0.5)
 
 if __name__ == '__main__':
